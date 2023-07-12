4cb33c4df4928067dcddebfff7d9238888cd736d
DJ1 <jimmynoble604@gmail.com>
2023-07-12 14:37:39 -0400

    completed

--- frontend/index.js
+++ frontend/index.js
@@ -6,6 +6,11 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import './styles/reset.css'
 import './styles/styles.css'
+import { Provider } from 'react-redux'
+import { resetStore } from './components/App'
+import { applyMibbleWare, CreateStore } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './state/reducer'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
