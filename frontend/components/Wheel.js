59f101a22ddfe985efb5a889bb11af42ca744a22
DJ1 <jimmynoble604@gmail.com>
2023-07-14 13:24:56 -0400

    wheel.js completed

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import { turnoffthemessage2, moveClockwise, moveCounterClockwise } from '../state/action-creators'
 
-export default function Wheel(props) {
+function Wheel(props) {
+  const { turnoffthemessage2, wheelState, moveClockwise, moveCounterClockwise } = props
+
+  useEffect(()=>{
+    turnoffthemessage2()
+  },[])
+
+  const handleclockwise = () => {
+    moveClockwise();
+  }
+
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+
+          {wheelState === 0 ? <div className="cog active" style={{ "--i": 0 }}>B</div>: <div className="cog" style={{ "--i": 0 }}></div>}
+          {wheelState === 1 ? <div className="cog active" style={{ "--i": 1 }}>B</div>: <div className="cog" style={{ "--i": 1 }}></div>}
+          {wheelState === 2 ? <div className="cog active" style={{ "--i": 2 }}>B</div>: <div className="cog" style={{ "--i": 2 }}></div>}
+          {wheelState === 3 ? <div className="cog active" style={{ "--i": 3 }}>B</div>: <div className="cog" style={{ "--i": 3 }}></div>}
+          {wheelState === 4 ? <div className="cog active" style={{ "--i": 4 }}>B</div>: <div className="cog" style={{ "--i": 4 }}></div>}
+          {wheelState === 5 ? <div className="cog active" style={{ "--i": 5 }}>B</div>: <div className="cog" style={{ "--i": 5 }}></div>}
+
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button onClick={handlecounterclock} id="counterClockwiseBtn" >Counter clockwise</button>
+        <button onClick={handleclockwise} id="clockwiseBtn">Clockwise</button>
       </div>
     </div>
   )
 }
+
+const mapStateToProps=(state) => {
+  return {
+    wheelState : state.wheel.initialWheelState
+  }
+}
+
+export default connect(mapStateToProps, {turnoffthemessage2, moveClockwise, moveCounterClockwise})(Wheel)
