6b5b92e1707cedbe42875c883d1659ccc2c6cdb5
DJ1 <jimmynoble604@gmail.com>
2023-07-13 22:35:11 -0400

    completed

--- frontend/components/Message.js
+++ frontend/components/Message.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function Message(props) {
-  return <div id="message">Nice job!</div>
+function Message(props) {
+  return (
+    <div id="message">
+      That was the correct answer
+      {props.inMessage.showmessage ? props.infoMessage.inmessageState : props.correctAnswer.testresponse}
+    </div>
+  )
 }
+
+export default connect(mstp => mstp, {})(Message);
+
