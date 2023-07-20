347628d89ff68f6e4881a1cd0ff6033c8cd62941
DJ1 <jimmynoble604@gmail.com>
2023-07-20 12:03:01 -0400

    errors fixed

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,89 +1,45 @@
-/* eslint-disable no-unused-vars */
+import * as types from "./action-types"
 // ❗ You don't need to add extra reducers to achieve MVP
-import {INPUT_CHANGE, MOVE_CLOCKWISE, MOVE_CONTERCLOCKWISE, RESET_FORM, SET_INFO_MESSAGE, SET_QUIZ_INTO_STATE, SET_SELECTED_ANSWER } from './action-types'
 import { combineReducers } from 'redux'
-import axios from 'axios'
-
-export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
 
 const initialWheelState = 0
-const wheel = (state = initialWheelState, action) => {
-  switch(action.type) {
-    case MOVE_CLOCKWISE:
-      if(state === 5) {
-        return state = initialWheelState
-      } else {
-        return state + 1
-      }
-    case MOVE_COUNTERCLOCKWISE: 
-      if(state === 0) {
-        return state = 5
-      } else {
-        return state - 1
-      }
+function wheel(state = initialWheelState, action) {
+  switch (action.type) {
+    case types.MOVE_CLOCKWISE: {
+      const nextIndex = state + 1
+      return nextIndex > 5 ? 0 : nextIndex
+    }
+    case types.MOVE_COUNTERCLOCKWISE: {
+      const nextIndex = state - 1
+      return nextIndex < 0 ? 5 : nextIndex
+    }
     default:
-      return state 
+      return state
   }
 }
 
 const initialQuizState = null
 function quiz(state = initialQuizState, action) {
-  switch(action.type) {
-    case SET_QUIZ_INTO_STATE:
-      if(action.payload === undefined) {
-        return( state = initialQuizState)
-      }
-    case SET_QUIZ_INTO_STATE: 
-      if(action.payload) {
-        return (state = action.payload)
-      }
-    default:
-      return state 
-      }
-  }
+  return state
+}
 
-  const initialSelectedAnswerState = null
-  function selectedAnswer(state = initialSelectedAnswerState, action) {
-    switch(action.type) {
-      case SET_SELECTED_ANSWER:
-        return state = action.payload
-      default:
-        return state 
-    }
-  }
+const initialSelectedAnswerState = null
+function selectedAnswer(state = initialSelectedAnswerState, action) {
+  return state
+}
 
-  const initialMessageState = ''
-  function infoMessage(state = initialMessageState, action) {
-    switch(action.type) {
-      case SET_INFO_MESSAGE:
-        if(action.payload === undefined) {
-          return state = initialMessageState
-        }
-      
+const initialMessageState = ''
+function infoMessage(state = initialMessageState, action) {
+  return state
+}
 
-      case SET_INFO_MESSAGE:
-        return state = action.payload
-      default:
-        return state 
-    }
-  
+const initialFormState = {
+  newQuestion: '',
+  newTrueAnswer: '',
+  newFalseAnswer: '',
+}
+function form(state = initialFormState, action) {
+  return state
+}
 
-  const initialFormState = {
-    newQuestion: '',
-    newTrueAnswer:'',
-    newFalseAnswer: '',
-  }
-  function form(state = initialFormState, action) {
-    switch(action.type) {
-      case INPUT_CHANGE:
-        return {
-          ...state,
-          [action.payload.id] : action.payload.value
-        }
-        case RESET_FORM:
-          return state = initialFormState
-          default:
-            return state 
-    }
-  }
-
+export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
